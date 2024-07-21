04f5eea67161e3e723148ae1e9ad95b05aaa1674
ruan-molinari <ruan.molinari@proton.me>
2024-07-21 11:47:30 -0300

    Declaring alacritty colors with nix-colors

--- home/base/gui/terminal/alacritty.nix
+++ home/base/gui/terminal/alacritty.nix
@@ -1,4 +1,9 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  config,
+  nix-colors,
+  ...
+}: {
 
   programs.alacritty = {
     enable = true;
@@ -11,7 +16,40 @@
         opacity = 0.8;
         blur = true;
       };
-    };
 
+      colors = {
+        primary = {
+          background = "#${config.colorScheme.palette.base00}";
+          foreground = "#${config.colorScheme.palette.base05}";
+        };
+
+        cursor = {
+          text = "#${config.colorScheme.palette.base00}";
+          cursor = "#${config.colorScheme.palette.base05}";
+        };
+
+        normal = {
+          black = "#${config.colorScheme.palette.base00}";
+          red = "#${config.colorScheme.palette.base08}";
+          green = "#${config.colorScheme.palette.base0B}";
+          yellow = "#${config.colorScheme.palette.base0A}";
+          blue = "#${config.colorScheme.palette.base0D}";
+          magenta = "#${config.colorScheme.palette.base0E}";
+          cyan = "#${config.colorScheme.palette.base0C}";
+          white = "#${config.colorScheme.palette.base05}";
+        };
+
+        bright = {
+          black = "#${config.colorScheme.palette.base03}";
+          red = "#${config.colorScheme.palette.base08}";
+          green = "#${config.colorScheme.palette.base0B}";
+          yellow = "#${config.colorScheme.palette.base0A}";
+          blue = "#${config.colorScheme.palette.base0D}";
+          magenta = "#${config.colorScheme.palette.base0E}";
+          cyan = "#${config.colorScheme.palette.base0C}";
+          white = "#${config.colorScheme.palette.base07}";
+        };
+      };
+    };
   };
 }
