9e4bcd352da3fd774ada32b28edf3562212fe0bc
ruan-molinari <ruan.molinari@proton.me>
2024-08-18 20:51:11 -0300

    Setting gtk theme as rose-pine

--- home/linux/gui/default.nix
+++ home/linux/gui/default.nix
@@ -1,5 +1,13 @@
-{ myLib, ... }: {
+{ myLib, pkgs, ... }: {
   imports = [
     ./river
   ];
+
+  gtk = {
+    enable = true;
+    theme = {
+      name = "rose-pine";
+      package = pkgs.rose-pine-gtk-theme;
+    };
+  };
 }
