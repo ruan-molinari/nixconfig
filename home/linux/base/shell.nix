0911a46a18ecf76635a9e766aeaa999d97731092
ruan-molinari <ruan.molinari@proton.me>
2024-08-27 22:40:05 -0300

    setting zen as default browser

--- home/linux/base/shell.nix
+++ home/linux/base/shell.nix
@@ -1,12 +1,11 @@
-{
-  config,
-  myVars,
-  ...
+{ config
+, myVars
+, ...
 }: {
   home.homeDirectory = "/home/${myVars.username}";
 
   # default environment variables go here
   home.sessionVariables = {
-    BROWSER = "firefox";
+    BROWSER = "zen";
   };
 }
